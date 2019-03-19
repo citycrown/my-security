2af1415fe762e730b24498b9f7ab74c9d14c955a
陈冕 <chenmian@rainbowcn.com>
2019-03-19 17:32:12 +0800

    first push

---
+++ src/main/java/com/my/security/MyApplication.java
@@ -0,0 +1,31 @@
+package com.my.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * 启动类
+ * <p>
+ *
+ * @author 陈冕
+ * @version 1.0.0
+ * @date 2019/3/19
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    private static final Logger log = LoggerFactory.getLogger("adminLogger");
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MyApplication.class, args);
+        org.springframework.core.env.Environment env = context.getEnvironment();
+        log.info("------");
+        log.info("当前环境：{}", env.getProperty("spring.profiles.active"));
+        log.info("执行成功");
+        log.info("------");
+    }
+
+}
---
+++ src/main/java/com/my/security/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.my.security.controller;
+
+/**
+ * user
+ * <p>
+ *
+ * @author 陈冕
+ * @version 1.0.0
+ * @date 2019/3/19
+ */
+public class UserController {
+}
