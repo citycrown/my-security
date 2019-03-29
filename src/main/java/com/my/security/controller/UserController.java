b92c48cba2ed511a84250b78eba8d96832c35111
陈冕 <chenmian@rainbowcn.com>
2019-03-29 17:07:09 +0800

    修改

--- src/main/java/com/my/security/controller/UserController.java
+++ src/main/java/com/my/security/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.my.security.controller;
 
+import com.my.security.entity.SysUserEntity;
+import com.my.security.mapper.SysUserMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +26,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger("adminLogger");
 
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+    @ApiOperation(value = "根据用户id获取用户")
+    @GetMapping("/{id}")
+    public SysUserEntity user(@PathVariable Integer id) {
+        return sysUserMapper.selectByPrimaryKey(id);
+    }
 
 }
