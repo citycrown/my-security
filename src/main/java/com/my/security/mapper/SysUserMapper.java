6c80f9ef3a4192a46cd0301856f7728abe78b669
citycrown <citycrown@126.com>
2019-04-04 23:00:34 +0800

    t

--- src/main/java/com/my/security/mapper/SysUserMapper.java
+++ src/main/java/com/my/security/mapper/SysUserMapper.java
@@ -1,7 +1,11 @@
 package com.my.security.mapper;
 
 import com.my.security.entity.SysUserEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Component
 public interface SysUserMapper {
     int deleteByPrimaryKey(Integer id);
 
